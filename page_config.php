319f5d079cb371dbbacbb4ba9b1b0f07b0744fd8
drewm <drew.mclellan@gmail.com>
2015-08-25 19:38:46 +0100

    Commenting

--- page_config.php
+++ page_config.php
@@ -1,39 +1,27 @@
 <?php
-
-	header('Content-Type: text/html; charset=utf-8');
-
 	/* Configuration vars for the page, passed through to the layout. */
 	$page_vars = array();
 
 	$page_vars['asset_version'] = 35;
 
-
-	#include('css.php');
-	#include('javascript.php');
-
-
-
 	/* The current year */
-	$page_vars['year'] = (int) date('Y');
-
+	$page_vars['year']       = (int) date('Y');
 	$page_vars['first_year'] = (int) 2005;
 
 
-
 	/* The year for the home page (and rss) */
-	if (time()>=strtotime('2014-11-01 00:00:00')) {
+	if (time()>=strtotime('2014-12-01 00:00:00')) { // switch over on Dec 1st!
 		$page_vars['home_year'] = 2014;
 	}else{
 		$page_vars['home_year'] = 2013;
 	}
 
+	// Let's not get ahead of ourselves
 	if ($page_vars['year'] > $page_vars['home_year']) $page_vars['year'] = $page_vars['home_year'];
 
 
 	$page_vars['theme'] = 'year-2014';
 	$page_vars['section'] = 'home';
-
-
 
 	/* Title of the page */
 	$page_vars['page_title']    = perch_pages_title(true);
